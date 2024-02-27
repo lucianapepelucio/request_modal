86fd5acc0109d4d03fa073d5d234b45fafd169f5
luciana.lucio <luciana.lucio@g.globo>
2024-02-27 18:48:44 -0300

    create modal component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import MuiDialogTitle from "@material-ui/core/DialogTitle";
+import MuiDialogContent from "@material-ui/core/DialogContent";
+import MuiDialogActions from "@material-ui/core/DialogActions";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import Typography from "@material-ui/core/Typography";
 
-function App() {
+const styles = (theme) => ({
+  titleModal: {
+    margin: 0,
+    padding: theme.spacing(3),
+  },
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(2),
+    top: theme.spacing(2),
+    color: theme.palette.grey[500],
+  },
+});
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <MuiDialogTitle disableTypography className={classes.titleModal} {...other}>
+      <Typography variant="h6">{children}</Typography>
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
         >
-          Learn React
-        </a>
-      </header>
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogActions);
+
+// const Avatar = withStyles((theme) => ({
+//   root: {
+//     padding: theme.spacing(2),
+//   },
+// }))();
+
+export default function CustomizedDialogs() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Solicitar edição do documento
+      </Button>
+      <Dialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+          Solicitando edição
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            {/* <div>
+              <img src="avatar.jpg"></img>
+            </div> */}
+            <strong>(nome do solicitante)</strong>, você está solicitando a
+            edição para <strong>(nome do editor)</strong>
+            <strong>(email do editor)</strong>. Pode levar algum tempo até sua
+            solicitação ser aceita.
+          </Typography>
+          <DialogActions>
+            <Button variant="outlined" onClick={handleClose} color="default">
+              Cancelar
+            </Button>
+            <Button variant="contained" color="primary">
+              Solicitar edição
+            </Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
 
-export default App;
+  
